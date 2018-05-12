066c03ade0d742ed171cb9d721adb0df502f243f
smashing <smashing@SMASHING-PC>
2018-05-12 14:47:21 +0200

    Map :p

--- BMOL/BMOL/Header.h
+++ BMOL/BMOL/Header.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <iostream>
 #include <string>
 #include <time.h>
@@ -38,6 +38,25 @@ private:
 	int iFirerange;		// Reichweite der Bombenexplosion
 	int iBombamount;	// Anzahl der Bomben die man tragen kann
 	float fBombtimer = 3; // 3 Sekunden bis zum explodieren der Bombe nach dem legen
+	// int BombType; // Spaeter um Bomben z haben die durch mauern gehen etc
 public:
 	void bombExplode();		// Funktion zum explodieren der Bombe
+};
+
+class MAP
+{
+	unsigned char *aField; // Zeiger damit man die Mapgroesse 'onthefly' ändern kann :p unsigned char weil wir nur 1 byte brauchen ( 255 werte für feldstatus.... )
+	int iSpecial; // Spezialeffekte
+	// char * Soundfile;
+	MAP( int x, int y, int effekt/* , char* Sound */ ) // Konstruktor...Reserviert Speicher für die Map je nach groesse..
+	{
+		aField = new unsigned char(x*y);
+		iSpecial = effekt;
+		// Zugriff auf einzelne felder kann später durch field[x][y] erfolgen dadurch wie mehrdimensionale arrays gespeichert werden
+		//int len=strlen( Sound );
+		// Soundfile = new char[ len+1 ];
+		// strcpy_s<len+1>( Soundfile, Sound );
+
+		// Map initialisieren mit Werten für Walkable etc...
+	}
 };
